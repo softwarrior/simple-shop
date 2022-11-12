c386dc3b51bb2bea2fd3f05a8c112d146943cb43
Andrey Pilipenko <Pilipenko.A.Bo@sberbank.ru>
2022-11-12 18:31:15 +0300

    поправил цвета у статуса

--- src/features/OrderListPage/components/OrdersTable/OrdersTableBody/Status/Status.jsx
+++ src/features/OrderListPage/components/OrdersTable/OrdersTableBody/Status/Status.jsx
@@ -18,23 +18,32 @@ const statusIcon = {
   declined: IconType.abort,
 };
 
-const statusStyle = {
-  new: styles.iconPostponed,
+const statusIconStyle = {
+  new: styles.iconNew,
   calculation: styles.iconCalculation,
-  confirmed: styles.iconCompleted,
+  confirmed: styles.iconConfirmed,
   postponed: styles.iconPostponed,
   completed: styles.iconCompleted,
-  declined: styles.iconCanceled,
+  declined: styles.iconDeclined,
+};
+
+const statusTextStyle = {
+  new: styles.textNew,
+  calculation: styles.textCalculation,
+  confirmed: styles.textConfirmed,
+  postponed: styles.textPostponed,
+  completed: styles.textCompleted,
+  declined: styles.textDeclined,
 };
 
 export const Status = ({ children }) => {
   return (
     <>
       <Icon
-        className={classnames(styles.icon, statusStyle[children])}
+        className={classnames(styles.icon, statusIconStyle[children])}
         iconType={statusIcon[children] || IconType.dot}
       />
-      <span className={classnames(styles.text, statusStyle[children])}>
+      <span className={classnames(styles.text, statusTextStyle[children])}>
         {statusName[children]}
       </span>
     </>
